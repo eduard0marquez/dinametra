325108152c3aab86b41df08818664e9acc5cec8a
eduard0marquez <edwartmarquez@gmail.com>
2025-10-22 19:12:43 -0600

    conexion con api y con tabla

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,28 +1,86 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Form, Table } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import { getData } from "../../helpers/Appi";
 
 function HomePage() {
-    const navigatee = useNavigate();
-    const Logout = () => {
-        localStorage.removeItem("user");
-      Swal.fire({
-        icon: "success",
-          title: `¡ ${`La sesión se cerro correctamente`}!`,
-        text: "Hasta pronto .",
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 3000,
-          timerProgressBar: true,
-        
+    const [objetoCercano,setObjetoCercano]=useState([])
+  const {
+    register,
+    handleSubmit,
+    formState: { error },
+    reset,
+  } = useForm();
+
+  const datos = (data) => {
+    const api = getData()
+        .then((response) => {
+            setObjetoCercano(response.near_earth_objects["2015-09-07"]);
+        console.log(response.near_earth_objects["2015-09-07"]);
+      })
+      .catch((error) => {
+        console.log(error);
       });
-      navigatee("/auth", { replace: true });
-    };
-    return <div>
-      <Button variant="danger" onClick={Logout}>Cerrar Sesión</Button>
-  </div>;
+  };
+
+  const navigatee = useNavigate();
+  const Logout = () => {
+    localStorage.removeItem("user");
+    Swal.fire({
+      icon: "success",
+      title: `¡ ${`La sesión se cerro correctamente`}!`,
+      text: "Hasta pronto .",
+      toast: true,
+      position: "top-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+    });
+    navigatee("/auth", { replace: true });
+  };
+  return (
+    <div>
+      <Form onSubmit={handleSubmit(datos)}>
+        <div>
+          <input type="date" {...register("fecha-inicio")} />
+        </div>
+
+        <input type="date" {...register("fecha-fin")} />
+        <Button variant="primary" type="submit">
+          Enviar
+        </Button>
+      </Form>
+      <div>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Nombre</th>
+                          <td>Diametro Estimado(KM)</td>
+                          <td>Es potencialmente peligroso</td>
+            </tr>
+          </thead>
+          <tbody>
+            {objetoCercano.map((objeto) => (
+              <tr>
+                    <td>{objeto.id}</td>
+                    <td>{objeto.name}</td>
+                    <td>{objeto.estimated_diameter.kilometers.estimated_diameter_max.toFixed(3)} KM</td>
+                    <td>{objeto.is_potentially_hazardous_asteroid ? "Sí" : "No"}</td>
+                    
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+
+      <Button variant="danger" onClick={Logout}>
+        Cerrar Sesión
+      </Button>
+    </div>
+  );
 }
 
 export default HomePage;
--- src/helpers/Appi.js
+++ src/helpers/Appi.js
@@ -1,21 +1,22 @@
-const url = "https://api.spotify.com";
-const token = "undefined";
+const url = "https://api.nasa.gov/neo/rest/v1/feed";
+const inicio = "2015-09-07";
+const finalizacion= "2015-09-08";
+const complemento = "NidmYzaSwjMuZ2lvIGwMdA3DZIpJt9LDDFPeK51n";
 
-export const getData = async (datos,complemento) => {
+export const getData = async (datos) => {
     try {
-        const resp = await fetch(`${url}/${complemento}`, {
-            method: "POST",
-            body: JSON.stringify(datos),
-            headers: {
-               Authorization: `Bearer ${token}`,
-            },
+        const resp = await fetch(`${url}?start_date=${inicio}&end_date=${finalizacion}&api_key=${complemento}`, {
+            method: "GET",
+            
         });
         const data = await resp.json();
         return data;
     }
     catch (error) {
+        console.log(`${url}?start_date=${inicio}&end_date=${finalizacion}&api_key=${complemento}`);
         console.error("Error:", error);
-        return {msg: "Error en la solicitud con la API"};
+        return { msg: "Error en la solicitud con la API" };
+        
 
     }
 }
